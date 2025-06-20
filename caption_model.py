3e557e6625b66b295864997e18f191591605b8e2
mouleshleo <mouleshleo06@gmail.com>
2025-06-20 17:43:46 +0530

    git-base model is better

--- caption_model.py
+++ caption_model.py
@@ -1,4 +1,4 @@
-from transformers import BlipProcessor, BlipForConditionalGeneration
+from transformers import AutoProcessor, AutoModelForCausalLM
 from PIL import Image
 import torch
 import time
@@ -6,23 +6,24 @@ import time
 class CaptionModel:
     def __init__(self):
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
-        self.processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
-        self.model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base").to(self.device)
+        self.processor = AutoProcessor.from_pretrained("microsoft/git-base")
+        self.model = AutoModelForCausalLM.from_pretrained("microsoft/git-base").to(self.device)
         self.model.eval()
 
-        print("[INFO] Warming up BLIP-1 caption model...")
+        print("[INFO] Warming up GIT-Base caption model...")
         _ = self.caption(Image.new("RGB", (224, 224)))
         print("[INFO] Warm-up complete ✅")
 
     def caption(self, image_pil):
-
         start = time.time()
+        image_pil = image_pil.resize((224, 224))
+
         inputs = self.processor(images=image_pil, return_tensors="pt").to(self.device)
-        
+
         with torch.no_grad():
             output = self.model.generate(**inputs, max_new_tokens=30)
-            
-        caption = self.processor.decode(output[0], skip_special_tokens=True)
+
+        caption = self.processor.batch_decode(output, skip_special_tokens=True)[0].strip()
         end = time.time()
 
         print(f"true caption time {end - start}")
